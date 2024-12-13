aba25d5f5f5957d4e86c8697d4d24ef2e577f3d0
yuki <yuki256f0111@gmail.com>
2024-12-13 14:03:13 +0900

    ipfs追加

--- main.js
+++ main.js
@@ -1,24 +1,43 @@
 const { app, BrowserWindow, ipcMain, dialog } = require('electron')
 const path = require('path')
+const { exec } = require('child_process');
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-        preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, 'preload.js'),
     },
   });
 
   // IPC handler
   ipcMain.handle('click-event', async (_e, _arg) => {
     const options = {
-        type: 'info',
-        title: 'quick start',
-        message: 'hello world!',
-        detail: 'description'
+      type: 'info',
+      title: 'quick start',
+      message: 'hello world!',
+      detail: 'description'
     };
     dialog.showMessageBox(options);
+  });
+
+  // IPC handler IPFS実行
+  ipcMain.handle('click-event', async (_e, _arg) => {
+    // 実行する.batファイルのパス
+    const batFilePath = 'path/to/your/script.bat';
+
+    exec(`"${batFilePath}"`, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`エラーが発生しました: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.error(`エラー出力: ${stderr}`);
+        return;
+      }
+      console.log(`出力: ${stdout}`);
+    });
   });
 
   ipcMain.on('close', () => {

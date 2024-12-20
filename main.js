c5904030dcbbb8e6fd600900f73b3d1344616710
yuki <yuki256f0111@gmail.com>
2024-12-20 16:52:06 +0900

    一般ノードとbootstrapノードでの起動機能

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const { app, BrowserWindow, ipcMain, dialog } = require('electron')
 const path = require('path')
-const { spawn } = require('child_process');
+const { spawn, execSync } = require('child_process');
 const iconv = require("iconv-lite");
+const fs = require('fs').promises;
+const os = require('os');
 
+let batProcess;
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -13,40 +16,129 @@ const createWindow = () => {
     },
   });
 
-  // IPC handler
+  //ファイルを書き換える
+  async function replaceLine(filePath, lineNumber, newText) {
+    try {
+      // ファイル全体を読み込む
+      const data = await fs.readFile(filePath, 'utf8');
+
+      // 行単位で分割
+      const lines = data.split('\n');
+
+      // 指定した行を更新
+      if (lineNumber >= 1 && lineNumber <= lines.length) {
+        lines[lineNumber - 1] = newText;
+      } else {
+        console.error('The specified line number is out of range.');
+        return;
+      }
+
+      // ファイルに書き戻す
+      await fs.writeFile(filePath, lines.join('\n'), 'utf8');
+      console.log('The specified line has been changed.');
+    } catch (err) {
+      console.error('An error has occurred:', err);
+    }
+  }
+
+
+  async function bootIPFS() {
+    require('child_process').exec('ipconfig', (err, stdout, stderr) => {
+
+      console.log('stdout:', stdout);
+      console.log('stderr:', stderr);
+    })
+
+    // 実行する.batファイルのパス
+    const batFilePath = 'run.bat';
+
+    batProcess = spawn('cmd.exe', ['/c', batFilePath]);
+
+    // 標準出力を取得
+    batProcess.stdout.on('data', (data) => {
+      console.log(`出力: ${iconv.decode(data, 'Shift_JIS')}`);
+    });
+
+    // 標準エラーを取得
+    batProcess.stderr.on('data', (data) => {
+      console.error(`エラー: ${iconv.decode(data, 'Shift_JIS')}`);
+    });
+
+    // プロセス終了時の処理
+    batProcess.on('close', (code) => {
+      console.log(`プロセスが終了しました。終了コード: ${code}`);
+    });
+
+  }
+
+
+  // IPC handler bootstrapノードで起動
   ipcMain.handle('click-event', async (_e, _arg) => {
-    const options = {
-      type: 'info',
-      title: 'quick start',
-      message: 'hello world!',
-      detail: 'description'
-    };
-    dialog.showMessageBox(options);
+
+
+    //bootstrapノード　kadrtt.propertiesと.ipfs/config内のBootstrapの部分の書き換え
+    async function setPropertiesConfig() {
+
+      let myip; //自身のIPアドレス
+      const networkInterfaces = os.networkInterfaces(); // ネットワークインターフェース情報を取得
+
+      for (const interfaceName in networkInterfaces) {
+        const interfaces = networkInterfaces[interfaceName];
+
+        for (const iface of interfaces) {
+          // IPv4かつ内部ネットワークでないアドレスを取得
+          if (iface.family === 'IPv4' && !iface.internal) {
+            console.log(`IPアドレス (${interfaceName}): ${iface.address}`);
+            myip = iface.address;
+          }
+        }
+      }
+
+      //生成された.ipfs/configのBootstrapID読み取り
+      let mypeerID;
+
+      await fs.readFile(".ipfs/config").then(file => {
+        const data = JSON.parse(file);
+        mypeerID = data.Identity.PeerID;
+        console.log(`MyBootstrapPeerID is ${mypeerID} !`);
+
+      }).catch(err => {
+        console.error(err);
+      });
+
+      replaceLine('kadrtt.properties', 36, `ipfs.endpoint=/ip4/${myip}/tcp/4001/ipfs/${mypeerID}`);
+      replaceLine('.ipfs/config', 13, `"/ip4/${myip}/tcp/4001/ipfs/${mypeerID}"`);
+    }
+
+    setPropertiesConfig();
+    bootIPFS();
   });
 
-  // // IPC handler IPFS実行
-  // ipcMain.handle('click-event', async (_e, _arg) => {
+  //IPC handler 一般ノード起動
+  ipcMain.handle('send-text', async (_e, bootstrapIp, bootstrapPeerId) => {
 
-  //   // 実行する.batファイルのパス
-  //   const batFilePath = '';
+    //config削除
+    function deleteFile(filePath) {
+      try {
+        fs.unlink(filePath);
+        return true;
+      } catch (err) {
+        return false;
+      }
+    }
 
-  //   const batProcess = spawn('cmd.exe', ['/c', batFilePath],{ shell: true, stdio: 'inherit' });
+    const configpath = path.join('.ipfs', 'config');
+    deleteFile(configpath)
 
-  //   // 標準出力を取得
-  //   batProcess.stdout.on('data', (data) => {
-  //     console.log(`出力: ${iconv.decode(data, 'UTF-8')}`);
-  //   });
+    console.log(`入力されたBootstrapのIp${bootstrapIp}`)
+    console.log(`入力されたBootstrapのPeerID${bootstrapPeerId}`)
 
-  //   // 標準エラーを取得
-  //   batProcess.stderr.on('data', (data) => {
-  //     console.error(`エラー: ${iconv.decode(data, 'UTF-8')}`);
-  //   });
+    //endpointに書き換える。
+    replaceLine('kadrtt.properties', 36, `ipfs.endpoint=/ip4/${bootstrapIp}/tcp/4001/ipfs/${bootstrapPeerId}`);
 
-  //   // プロセス終了時の処理
-  //   batProcess.on('close', (code) => {
-  //     console.log(`プロセスが終了しました。終了コード: ${code}`);
-  //   });
-  // });
+    bootIPFS();
+  })
+
 
   ipcMain.on('close', () => {
     app.quit();
@@ -56,6 +148,8 @@ const createWindow = () => {
 
   //win.webContents.openDevTools();
 };
+
+
 
 app.whenReady().then(() => {
   createWindow();
@@ -69,6 +163,17 @@ app.whenReady().then(() => {
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
+
+    //ipfsを終了させる。
+    console.log('タスクを同期的に終了します...');
+    try {
+      const result = execSync(`taskkill /PID ${batProcess.pid} /T /F`, { encoding: 'utf-8' });
+      console.log(`出力: ${result}`);
+    } catch (error) {
+      console.error(`エラー: ${error.message}`);
+    }
+    
+    console.log("test2");
     app.quit();
   }
 });
--- preload.js
+++ preload.js
@@ -3,3 +3,15 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('apis', {
     runClickEvent: async () => ipcRenderer.invoke('click-event'),
 });
+
+contextBridge.exposeInMainWorld('apis2', {
+    sendText: (testBoxBTIP,testBoxBTPeerID) => ipcRenderer.invoke('send-text', testBoxBTIP,testBoxBTPeerID),
+  });
+
+contextBridge.exposeInMainWorld('apis3', {
+    runClickEvent: async () => ipcRenderer.invoke('click-event3'),
+});
+
+contextBridge.exposeInMainWorld('apis4', {
+    runClickEvent: async () => ipcRenderer.invoke('click-event4'),
+});
